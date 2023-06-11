35a7301d3bb99aa092fddffc6a9b09afd00b7c3b
Mymai0511 <116066801+Mymai0511@users.noreply.github.com>
2023-06-11 17:24:25 +0700

    Đẩy category từ sql lên jsp

---
+++ Web_ShopCaiBanh/src/java/DAL/CategoryDAO.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAL;
+
+import Model.Category;
+import java.util.List;
+import java.sql.PreparedStatement;// lệnh có đối số
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.naming.spi.DirStateFactory;
+
+/**
+ *
+ * @author DELL
+ */
+public class CategoryDAO extends DBContext {
+
+    //lấy tất cả dữ liệu từ bảng category
+    //select * from Category
+    public List<Category> getAll() {
+        List<Category> list = new ArrayList<>();
+        String sql = "select * from Category";
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);// kết nối với sql
+            ResultSet rs = st.executeQuery();// trả về kết quả từ sql- nhiều bản ghi
+            while(rs.next()){
+                Category c = new Category();
+                c.setId(rs.getInt("category_id"));
+                c.setName(rs.getString("category_name"));
+                list.add(c);
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return list;
+    }
+//    
+//    //test có kết nối được với database không
+//    public static void main(String[] args) {
+//        CategoryDAO c = new CategoryDAO();
+//        List<Category> list = c.getAll();
+//        System.out.println(list.get(0).getName());
+//    }
+
+}
